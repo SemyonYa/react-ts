f45cfe2e98f5b153ae5123a8f3bfdcee4a963c7a
SemyonYa <mygarbage86@mail.ru>
2021-07-15 18:07:39 +0300

    updtd 2

--- src/_RouteViewer/Item.ts
+++ src/_RouteViewer/Item.ts
@@ -1,12 +1,13 @@
 import React from "react";
 import { RoutePartDetailsDTO } from "./RoutePartDetailsDTO";
+import { FetchStatus } from "./RouteViewer";
 
 interface IItemProps {
     route: RoutePartDetailsDTO
 }
 
 interface IItemState {
-    expanded: boolean;
+    status: FetchStatus;
 }
 
 export class Item extends React.PureComponent<IItemProps, IItemState> {
@@ -14,23 +15,64 @@ export class Item extends React.PureComponent<IItemProps, IItemState> {
     constructor(props: IItemProps) {
         super(props);
         this.state = {
-            expanded: false
+            status: FetchStatus.Initial
         }
     }
 
     render() {
+        let component: React.ReactNode;
+        switch (this.state.status) {
+            case FetchStatus.InProgress:
+                component = React.createElement('span', {}, 'in progress');
+                break;
+            case FetchStatus.Fetched:
+                component = this.props.route.children && this.props.route.children.length > 0
+                    ? React.Children.toArray(this.props.route.children.map(route =>
+                        React.createElement(Item, { route })
+                    ))
+                    : null
+                break;
+            case FetchStatus.Failed:
+                component = React.createElement('span', {}, 'failed');
+                break;
+            default:
+                component = null;
+                break;
+        }
         return (
             React.createElement('div', { style: { display: 'flex', flexDirection: 'column' } },
-                React.createElement('div', {}, this.props.route.name),
-                React.createElement('div', { style: { display: 'flex', flexDirection: 'column', paddingLeft: '2rem' } },
-                    this.props.route.children && this.props.route.children.length > 0
-                        ? React.Children.toArray(this.props.route.children.map(route =>
-
-                            React.createElement('div', {}, route.name)
-                        ))
-                        : null
-                ),
+                React.createElement('div', { onClick: this.fetchCHildren }, this.props.route.name),
+                React.createElement('div', { style: { display: 'flex', flexDirection: 'column', paddingLeft: '2rem' } }, component),
             )
         );
+    }
+
+    fetchCHildren = () => {
+        this.setState({ status: FetchStatus.InProgress });
+        // TODO: /api/Configuration/Routes/{elementId}
+        this._children(this.props.route.id).then(
+            children => {
+                this.props.route.children = children;
+                this.setState({ status: FetchStatus.Fetched });
+                this.forceUpdate();
+            },
+            reason => {
+                this.setState({ status: FetchStatus.Failed });
+            }
+        )
+    }
+
+
+    /// FAKE
+    private _children(parentId: string): Promise<RoutePartDetailsDTO[]> {
+        return new Promise<RoutePartDetailsDTO[]>((resolve, reject) => {
+            setTimeout(() => {
+                resolve([1, 2, 3].map(
+                    i => {
+                        return { id: `${parentId}-${i}`, name: `Route ${parentId}-${i}`, isPage: false, children: [] } as RoutePartDetailsDTO;
+                    }
+                ));
+            }, 2000);
+        })
     }
 }
--- src/_RouteViewer/RouteViewer.ts
+++ src/_RouteViewer/RouteViewer.ts
@@ -4,11 +4,20 @@ import { Item } from './Item';
 import { Pagination } from './Pagination';
 import { RoutePartDetailsDTO } from './RoutePartDetailsDTO';
 
+
+export enum FetchStatus {
+    Initial,
+    InProgress,
+    Fetched,
+    Failed
+}
+
 interface IRouterViewState {
     searchValue: string;
     pageSize: number;
     pageNumber: number;
-    routes: RoutePartDetailsDTO[]
+    routes: RoutePartDetailsDTO[],
+    status: FetchStatus
 }
 
 export class RouteViewer extends React.PureComponent<ISectionProps, IRouterViewState> {
@@ -18,9 +27,10 @@ export class RouteViewer extends React.PureComponent<ISectionProps, IRouterViewS
         super(props);
         this.state = {
             searchValue: '',
-            pageSize: 10,
+            pageSize: 2,
             pageNumber: 1,
-            routes: []
+            routes: [],
+            status: FetchStatus.InProgress
         };
     }
 
@@ -29,7 +39,9 @@ export class RouteViewer extends React.PureComponent<ISectionProps, IRouterViewS
     }
 
     private changePage = (page: number) => {
+        // TODO: 
         this.setState({ pageNumber: page })
+        this.fetchRoutes(page);
     }
 
     private onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -37,82 +49,108 @@ export class RouteViewer extends React.PureComponent<ISectionProps, IRouterViewS
         this.setState({ searchValue: currentValue });
         if (this.currentTimeout) clearTimeout(this.currentTimeout);
         this.currentTimeout = setTimeout(() => {
-            console.log('TODO: search', currentValue);
+            this.setState({ status: FetchStatus.InProgress });
+            // TODO: /api/Configuration/Routes/search/{searchText}
+            this._search(currentValue?.trim()).then(
+                routes => {
+                    this.setState({ routes, pageNumber: 1, status: FetchStatus.Fetched })
+                }
+            );
         }, 500);
     }
 
     private onPageSizeSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.code === 'Enter' || e.code === 'NumpadEnter') {
             const currentValue: number = +e.currentTarget.value;
-            // this.setState({ pageSize: currentValue });
-            this.fetchRoutes(this.state.pageNumber, currentValue);
-            // console.log('TODO: page size', currentValue);
+            this.setState({ pageSize: currentValue });
+            this.fetchRoutes(1);
         }
     }
 
-    private fetchRoutes = (pageNumber: number = 1, pageSize: number = 10) => {
-        // TODO: Real data
-        this._list(pageNumber, pageSize).then(
+    private fetchRoutes = (pageNumber: number = null) => {
+        // TODO: fetch states
+        this.setState({ status: FetchStatus.InProgress });
+        // TODO: /api/Configuration/Routes?pageNumber={pageNumber}&pageSize={pageSize
+        this._list(pageNumber ?? this.state.pageNumber, this.state.pageSize).then(
             (routes) => {
-                this.setState({ routes, pageSize, pageNumber })
+                this.setState({ routes, status: FetchStatus.Fetched })
             }
         );
     }
 
     render() {
+        let component;
+        switch (this.state.status) {
+            case FetchStatus.InProgress:
+                component = React.createElement('span', {}, 'in progress');
+                break;
+            case FetchStatus.Fetched:
+                component = this.state.routes.length > 0
+                    ? React.Children.toArray(this.state.routes.map(route =>
+                        React.createElement(Item, { route }),
+                    ))
+                    : React.createElement('span', {}, 'empty list');
+                break;
+            case FetchStatus.Failed:
+                component = React.createElement('span', {}, 'failed');
+                break;
+            default:
+                component = null;
+                break;
+        }
         return (
             React.createElement('div', { style: { display: 'flex', flexDirection: 'column', maxWidth: '25rem', marginTop: '5rem' } },
                 React.createElement('input', { onChange: this.onSearchChange, value: this.state.searchValue, placeholder: 'search' }),
-                React.createElement('div', { style: { display: 'flex', flexDirection: 'column' } },
-                    this.state.routes.length > 0
-                        ? React.Children.toArray(this.state.routes.map(route =>
-                            React.createElement(Item, { route }),
-                        ))
-                        : React.createElement('span', {}, 'empty list'),
-                ),
+                React.createElement('div', { style: { display: 'flex', flexDirection: 'column' } }, component),
+                (this.state.status === FetchStatus.Fetched)
+                    ? React.createElement('a', { href: '/Admin/PageEditor' }, 'Новая страница')
+                    : null,
                 React.createElement('input', { onKeyDown: this.onPageSizeSubmit, placeholder: 'page size', type: 'number' }),
-                React.createElement(Pagination, { onChange: this.changePage, pageQty: 10, pageNumber: this.state.pageNumber })
+                React.createElement(Pagination, {
+                    onChange: this.changePage,
+                    // TODO: routes.lenth - incorrect. API response...
+                    pageQty: Math.ceil(this.state.routes.length / this.state.pageSize),
+                    pageNumber: this.state.pageNumber
+                })
             )
         );
     }
 
-    ///
-    ///
+
     ///
     /// FAKE REQUESTS
     private _list(pageNumber: number = 1, pageSize: number = 10): Promise<RoutePartDetailsDTO[]> {
         return new Promise<RoutePartDetailsDTO[]>((resolve, reject) => {
             setTimeout(() => {
                 resolve([
-                    { id: 'dqwd', name: 'Route 1', isPage: false, children: [] } as RoutePartDetailsDTO,
-                    {
-                        id: 'dqwd', name: 'Route 2', isPage: false, children: [
-                            { id: 'dqwd', name: 'Route 2-1', isPage: false, children: [] } as RoutePartDetailsDTO,
-                            {
-                                id: 'dqwd', name: 'Route 2-2', isPage: false, children: [
-                                    { id: 'dqwd', name: 'Route 2-2-1', isPage: false, children: [] } as RoutePartDetailsDTO,
-                                    { id: 'dqwd', name: 'Route 2-2-2', isPage: false, children: [] } as RoutePartDetailsDTO,
-                                    { id: 'dqwd', name: 'Route 2-2-3', isPage: false, children: [] } as RoutePartDetailsDTO
-                                ]
-                            } as RoutePartDetailsDTO
-                        ]
-                    } as RoutePartDetailsDTO
+                    { id: '1', name: 'Route 1', isPage: false, children: [] } as RoutePartDetailsDTO,
+                    { id: '2', name: 'Route 2', isPage: false, children: [] } as RoutePartDetailsDTO,
+                    { id: '3', name: 'Route 3', isPage: false, children: [] } as RoutePartDetailsDTO,
                 ]);
             }, 2000);
         })
     }
 
-    private _children(parentId: string): Promise<RoutePartDetailsDTO[]> {
-        return new Promise<RoutePartDetailsDTO[]>((resolve, reject) => {
-            setTimeout(() => {
-                resolve([]);
-            }, 2000);
-        })
-    }
     private _search(search: string): Promise<RoutePartDetailsDTO[]> {
         return new Promise<RoutePartDetailsDTO[]>((resolve, reject) => {
             setTimeout(() => {
-                resolve([]);
+                resolve([
+                    {
+                        id: '1', name: 'Route 1', isPage: false, children: [
+                            {
+                                id: '1-1', name: 'Route 2', isPage: false, children: [
+                                    { id: '1-1-1', name: `${search} - third level`, isPage: false, children: [] } as RoutePartDetailsDTO,
+                                ]
+                            } as RoutePartDetailsDTO,
+                        ]
+                    } as RoutePartDetailsDTO,
+                    {
+                        id: '2', name: 'Route 2', isPage: false, children: [
+                            { id: '2-1', name: `${search} - second level`, isPage: false, children: [] } as RoutePartDetailsDTO,
+                        ]
+                    } as RoutePartDetailsDTO,
+                    { id: '3', name: `${search} - top level`, isPage: false, children: [] } as RoutePartDetailsDTO,
+                ]);
             }, 2000);
         })
     }
