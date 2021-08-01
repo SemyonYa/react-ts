ff7fb737bd0ccd2142c7b19a8b444f9f3a6c27db
Semyon Ya <mygarbage86@mail.ru>
2021-08-01 23:42:09 +0300

    some methods

--- src/App.ts
+++ src/App.ts
@@ -65,7 +65,7 @@ export class App extends React.PureComponent<{}, IAppState> {
             ),
             React.createElement('h2', {}, 'My calculation session'),
             React.createElement('div', { style: { margin: '10px 0' } },
-              React.createElement(Button, { onClick: this.showModal }, 'SHOW MODAL')
+              React.createElement(Button, { onClick: this.showModal, text: 'SHOW MODAL' }, )
             ),
             React.createElement('div', { style: { backgroundColor: '#E5EDF5', borderRadius: '4px', boxShadow: 'inset 0px 2px 4px 0 rgba(0, 0, 0, .15)', padding: '25px' } }, this.lorem1),
             React.createElement('h2', {}, 'Other calculation sessions'),
--- src/components/_Button.ts
+++ src/components/_Button.ts
@@ -1,12 +1,30 @@
 import * as React from 'react';
-export interface IButtonProps {
-    onClick(): void
+
+interface IButtonProps {
+    text: string,
+    methodName?: string;
+    model?: any;
+    onClick(): void,
+}
+interface IButtonState {
+    value: boolean;
 }
 
 export class Button extends React.Component<IButtonProps> {
+
+    constructor(props: IButtonProps) {
+        super(props);
+        this.state = { value: false }
+    }
+
+    private click = () => {
+        this.props.onClick();
+        (this.props.model[this.props.methodName] as Function).call(this.props.model);
+    }
+
     render() {
         return (
-            React.createElement('button', { onClick: this.props.onClick }, this.props.children)
+            React.createElement('button', { onClick: this.props.onClick }, this.props.text)
         );
     }
 }
